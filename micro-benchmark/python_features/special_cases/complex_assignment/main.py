37c8e11b42e19e420bae3b4eb703630ecddaa669
Samzcodez <100213187+Samzcodez@users.noreply.github.com>
2023-05-10 02:21:07 +0200

    Correction | python features | special_cases | complex_asssignment - example instance

--- micro-benchmark/python_features/special_cases/complex_assignment/main.py
+++ micro-benchmark/python_features/special_cases/complex_assignment/main.py
@@ -1,8 +1,12 @@
 # A function with complex assignment
 def unique_substring(input_string):
     substrings = [input_string[i : i + 3] for i in range(len(input_string) - 2)]
+    print(substrings)
     unique = [x for x in substrings if len(set(x)) == 3]
-    return unique[0]
+    if unique:
+        return unique[0]
+    else:
+        return None
 
 
-c = unique_substring("aaabbbccc")
+c = unique_substring("aaabcbbccc")
