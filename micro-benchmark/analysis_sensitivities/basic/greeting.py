e75c202a4d5f85bf424d3b6daf88894123ecaba8
samkutty <samkutty@mail.uni-paderborn.de>
2023-05-15 21:22:48 +0200

    Deletion | sensitivity | greetings.py

--- micro-benchmark/analysis_sensitivities/basic/greeting.py
+++
@@ -1,6 +0,0 @@
-# A simple Python program that defines a function called 'greeting' which returns a greeting message.
-def greeting(string):
-    return "Hello, " + string + "!"
-
-
-result = greeting("World")
