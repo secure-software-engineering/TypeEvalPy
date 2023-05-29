c5034e2be70c36ca423ad49649d987002f16bdb6
Samzcodez <100213187+Samzcodez@users.noreply.github.com>
2023-05-29 20:35:25 +0200

    ground truths |python features | external

---
+++ scripts/scalpel_wrapper.py
@@ -0,0 +1,40 @@
+import json
+import os
+
+from scalpel.typeinfer.typeinfer import TypeInference
+
+
+def list_python_files(folder_path):
+    python_files = []
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(".py"):
+                python_files.append(os.path.join(root, file))
+    return python_files
+
+
+folder_path = (
+    "/home/sam/master-thesis-of-samkutty/micro-benchmark/python_features/external"
+)
+
+python_files = list_python_files(folder_path)
+
+for file in python_files:
+    print(file)
+    inferer = TypeInference(name=file, entry_point=file)
+    inferer.infer_types()
+    inferred = inferer.get_types()
+    print(inferred)
+    json_file_path = file.replace(".py", "_gt.json")
+
+    if os.path.exists(json_file_path):
+        print(f"JSON file already exists: {json_file_path}")
+        continue
+
+    with open(json_file_path, "w") as json_file:
+        # json.dump(inferred, json_file)
+        inferred_serializable = [
+            {k: list(v) if isinstance(v, set) else v for k, v in d.items()}
+            for d in inferred
+        ]
+        json.dump(inferred_serializable, json_file, indent=4)
