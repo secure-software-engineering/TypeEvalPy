c55cd251d1476cb0a4fff62c66cc31344e8bddf2
Samzcodes <samkutty@mail.uni-paderborn.de>
2023-06-28 13:20:56 +0200

    Tool scripts | Enhancing pytype script with get_annotations_list

--- src/tests_runner/pytype_script.py
+++ src/tests_runner/pytype_script.py
@@ -1,5 +1,8 @@
+import ast
+import json
 import os
-import subprocess
+from pytype import config
+from pytype.tools.annotate_ast import annotate_ast
 
 
 def list_python_files(folder_path):
@@ -11,23 +14,82 @@ def list_python_files(folder_path):
     return python_files
 
 
+def get_annotations_list(module, file):
+    results = []
+    current_class = None
+    for node in ast.walk(module):
+        if isinstance(node, ast.ClassDef):
+            current_class = node.name
+            # print(current_class)
+        if hasattr(node, "resolved_type"):
+            if isinstance(node, ast.FunctionDef):
+                function = {
+                    "file": file,
+                    "line_number": node.lineno,
+                    "function": node.name,
+                    "type": [node.resolved_annotation],
+                }
+                if current_class:
+                    function["function"] = f"{current_class}.{node.name}"
+                results.append(function)
+                for param in node.args.args:
+                    parameter = {
+                        "file": file,
+                        "line_number": param.lineno,
+                        "parameter": param.arg,
+                        "function": node.name,
+                        "type": [param.annotation],
+                    }
+                    if current_class:
+                        function["function"] = f"{current_class}.{node.name}"
+                    results.append(parameter)
+            elif isinstance(node, ast.Attribute):
+                print(node.attr)
+                attribute = {
+                    "file": file,
+                    "line_number": node.lineno,
+                    "variable": node.attr,
+                    "type": [node.resolved_annotation],
+                }
+                if current_class:
+                    attribute["variable"] = f"{current_class}.{node.attr}"
+                results.append(attribute)
+
+            elif isinstance(node, ast.Name):
+                variable = {
+                    "file": file,
+                    "line_number": node.lineno,
+                    "variable": node.id,
+                    "type": [node.resolved_annotation],
+                }
+                results.append(variable)
+            else:
+                if hasattr(node, "id"):
+                    print("Unkown object:", node.id)
+                else:
+                    print("Unknown node type")
+    return results
+
+
 os.chdir("/tmp")
-root_directory = "./micro-benchmark/python_features"
+folder_path = "./micro-benchmark"
 
-# Run `pytype' in the root directory for node installation
-
-python_files = list_python_files(root_directory)
+python_files = list_python_files(folder_path)
+pytype_options = config.Options.create(python_version=(3, 10))
 error_count = 0
-# Run `pytype` for all files
-for file_path in python_files:
-    print(file_path)
-    dir_path, file_name = os.path.split(file_path)
-    pytype_stub = (
-        "pytype -k --precise-return  --protocols --strict-parameter-checks"
-        f" '{file_name}'"
-    )
+for file in python_files:
     try:
-        subprocess.run(pytype_stub, shell=True, cwd=dir_path, check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Command returned non-zero exit status: {e.returncode}")
+        print(file)
+        dir_path, file_name = os.path.split(file)
+        source = open(file).read()
+        module = annotate_ast.annotate_source(source, ast, pytype_options)
+        annotations_list = get_annotations_list(module, file_name)
+        json_output = json.dumps(annotations_list, indent=4)
+        # print(json_output)
+        json_file_path = file.replace(".py", "_pytype_new.json")
+        # print(json_file_path)
+        with open(json_file_path, "w") as json_file:
+            json.dump(annotations_list, json_file, indent=4)
+    except Exception as e:
+        print(e)
         error_count += 1
