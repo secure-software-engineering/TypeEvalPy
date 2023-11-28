c770528639bdecb6cf7930fe37c450b44cd8e994
Ashwin Prasad <ashwinprasad.me@gmail.com>
2023-11-28 18:26:00 +0100

    Updated prompts and refactoring

--- src/target_tools/ollama/src/prompts.py
+++ src/target_tools/ollama/src/prompts.py
@@ -205,7 +205,6 @@ Your Task: Provide inferred types in JSON format for the above Python code, by a
 ```
 """
 
-
 typeevalpy_prompt_1_template = """
 You will be provided with the following information: 
 1. Python code. The sample is delimited with triple backticks. 
@@ -224,7 +223,7 @@ def id_func ( arg ):
     x = arg
     return x
  
-result = id_func (" String ")
+result = id_func ("String")
 result = id_func (1)
 ``` 
 
@@ -285,4 +284,194 @@ Python code:
 ``` 
 
 The JSON object:
+"""
+
+typeevalpy_prompt_2_template = """
+You will be provided with the following information: 
+1. Python code. The sample is delimited with triple backticks. 
+2. Sample JSON containing type inference information for the Python code in a specific format. 
+3. Examples of Python code and their inferred types. The examples are delimited with triple backticks. These examples are to be used as training data. 
+
+Perform the following tasks: 
+1. Infer the types of various Python elements like function parameters, local variables, and function return types according to the given JSON format with the highest probability. 
+2. Provide your response in a valid JSON array of objects according to the training sample given. Do not provide any additional information except the JSON object. 
+3. {format_instructions}
+
+
+Python code: 
+```
+def id_func ( arg ):
+    x = arg
+    return x
+ 
+result = id_func ("String")
+result = id_func (1)
+``` 
+
+The JSON object:
+```
+[
+   {{
+      "file": "simple_code.py",
+      "function": "id_func",
+      "line_number": 1,
+      "type": [
+         "int",
+         "str"
+      ]
+   }},
+   {{
+      "file": "simple_code.py",
+      "function": "id_func",
+      "line_number": 1,
+      "parameter": "arg",
+      "type": [
+         "int",
+         "str"
+      ]
+   }},
+   {{
+      "file": "simple_code.py",
+      "function": "id_func",
+      "line_number": 2,
+      "type":[
+         "int",
+         "str"
+      ],
+      "variable": "x"
+   }},
+   {{
+      "file": "simple_code.py",
+      "line_number": 5,
+      "type": [
+         "str"
+      ],
+      "variable": "result"
+   }},
+   {{
+      "file": "simple_code.py",
+      "line_number": 6,
+      "type":[
+         "int"
+      ],
+      "variable": "result"
+   }}
+]
+```
+
+Python code: 
+```
+{code}
+``` 
+
+Your Task: Provide inferred types in JSON format for the above Python code, by adding the appropriate 'type' keys to the following JSON object:
+```
+{json}
+```
+"""
+
+json_based_1 = """
+## Task Description
+
+You are required to analyze Python code samples and infer the types of different elements (e.g., function parameters, local variables, function return types) based on provided JSON schema. Your responses should adhere strictly to the specified JSON format.
+
+## Information Provided
+
+1. Python Code: Enclosed within triple backticks (```).
+2. JSON Schema: Outlines the format for type inference.
+3. Training Data: Examples of Python code with their inferred types, also within triple backticks.
+
+## Tasks to Perform
+
+1. Type Inference: Based on the provided JSON format, infer the types of various elements in the Python code (function parameters, local variables, function return types) with high accuracy.
+2. JSON Response: Provide your inferences in a JSON array of objects, strictly following the training sample format. Exclude any additional information outside of this JSON object.
+3. Adherence to JSON Schema: Ensure the output is a valid JSON instance conforming to the provided JSON schema.
+
+## Example:
+
+Python code: 
+```
+def id_func (arg):
+    x = arg
+    return x
+ 
+result = id_func ("String")
+result = id_func (1)
+``` 
+
+Answer as JSON object:
+```
+[
+   {{
+      "file": "simple_code.py",
+      "function": "id_func",
+      "line_number": 1,
+      "type": [
+         "int",
+         "str"
+      ]
+   }},
+   {{
+      "file": "simple_code.py",
+      "function": "id_func",
+      "line_number": 1,
+      "parameter": "arg",
+      "type": [
+         "int",
+         "str"
+      ]
+   }},
+   {{
+      "file": "simple_code.py",
+      "function": "id_func",
+      "line_number": 2,
+      "type":[
+         "int",
+         "str"
+      ],
+      "variable": "x"
+   }},
+   {{
+      "file": "simple_code.py",
+      "line_number": 5,
+      "type": [
+         "str"
+      ],
+      "variable": "result"
+   }},
+   {{
+      "file": "simple_code.py",
+      "line_number": 6,
+      "type":[
+         "int"
+      ],
+      "variable": "result"
+   }}
+]
+```
+
+## Your Current Task
+
+Infer types for the following Python code and provide a JSON response based on the given schema. 
+
+{format_instructions}
+
+Python Code:
+```
+{code}
+``` 
+
+Answer as JSON object:
+"""
+
+questions_based_1 = """
+## Task Description
+
+Analyze the provided Python code and determine the types of various elements. Answer the following questions based on your analysis.
+
+Questions:
+{questions}
+
+Python Code:
+{code}
 """
--- src/target_tools/ollama/src/utils.py
+++ src/target_tools/ollama/src/utils.py
@@ -1,5 +1,6 @@
 import os
 import json
+import sys
 
 
 def is_running_in_docker():
@@ -23,5 +24,55 @@ def generate_json_file(filename, type_info):
         print("Not a valid JSON")
 
     json_data = json.dumps(type_info, indent=4)
-    with open(filename, "a") as file:
+    with open(filename, "w") as file:
         file.write(json_data)
+
+
+def generate_questions_from_json(json_file):
+    # Read and parse the JSON file
+    with open(json_file, "r") as file:
+        data = json.load(file)
+
+    questions = []
+
+    for entry in data:
+        file = entry["file"]
+        line_number = entry["line_number"]
+        col_offset = entry["col_offset"]
+
+        # Generate different questions based on the content of each entry
+        # Function Return type
+        if "function" in entry and "parameter" not in entry and "variable" not in entry:
+            question = (
+                "What is the return type of the function"
+                f" '{entry['function']}' at line {line_number}, column"
+                f" {col_offset}?"
+            )
+        # Function Parameter type
+        elif "parameter" in entry:
+            question = (
+                f"What is the type of the parameter '{entry['parameter']}' at line"
+                f" {line_number}, column {col_offset}, within the function"
+                f" '{entry['function']}'?"
+            )
+        # Variable in a function type
+        elif "variable" in entry and "function" not in entry:
+            question = (
+                f"What is the type of the variable '{entry['variable']}' at line"
+                f" {line_number}, column {col_offset}?"
+            )
+        elif "variable" in entry and "function" in entry:
+            question = (
+                f"What is the type of the variable '{entry['variable']}' at line"
+                f" {line_number}, column {col_offset}, within the function"
+                f" '{entry['function']}'?"
+            )
+        else:
+            print("ERROR! Type could not be converted to types")
+        questions.append(question)
+
+    if len(data) != len(questions):
+        print("ERROR! Type questions length does not match json length")
+        sys.exit(-1)
+
+    return questions
